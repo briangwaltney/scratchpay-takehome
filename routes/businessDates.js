140b8f778e250ecfae8803c365ecfeccb325f935
Brian Gwaltney <brian.gwaltney@gmail.com>
2019-08-08 16:48:36 +0700

    fixed validation error

--- routes/businessDates.js
+++ routes/businessDates.js
@@ -1,24 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { validate } = require('../models/businessDates');
-const validateBody = require('../middleware/validateBody');
-const calculateDays = require('../utilities/calculateDays')
+const Joi = require("joi");
+//const { validate } = require("../models/businessDates");
+const validateBody = require("../middleware/validateBody");
+const calculateDays = require("../utilities/calculateDays");
 
+//Validation function used to validate API endpoint inputs.
+function validate(query) {
+  const schema = {
+    initialDate: Joi.date().required(),
+    delay: Joi.number().required()
+  };
+  return Joi.validate(query, schema);
+}
 
 //API endpoint used for all calls including GET and POST as requested in the challenge.
-router.all('/', validateBody(validate), async (req, res) => {
-  //error handling is handled by validateBody middleware. 
+router.all("/", validateBody(validate), async (req, res) => {
+  //error handling is handled by validateBody middleware.
 
-  const results = calculateDays(req.body)
+  const results = calculateDays(req.body);
 
   return res.status(200).send({
     ok: true,
     initialQuery: req.body,
     results
-  })
+  });
+});
 
-})
-
-
-module.exports = router
-
+module.exports = router;
